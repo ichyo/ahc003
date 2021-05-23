859db292119cdaa138cc63fcaacb87acd2d6f5db
ichyo <ichyo00@gmail.com>
2021-05-23 15:44:13 +0900

    Tweak features

--- bundle.sh
+++ bundle.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cargo equip --bin submission --exclude-atcoder-crates --rustfmt --remove docs --remove comments
+cd ./submission
+cargo equip --exclude-atcoder-crates --rustfmt --remove docs --remove comments
